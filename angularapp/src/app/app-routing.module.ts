626009f02f93604fab3beee96d101c107178fbb2
rishabh-karmakar <rishabh.rk1705@gmail.com>
2021-10-05 13:55:59 +0000

    Added base angular and springboot

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './security/auth-guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:RegistrationFormComponent
+  },
+  {
+    path:'login',
+    component:LoginFormComponent
+  },
+  {
+    path:'dashboard',
+    component:DashboardComponent,
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,16 +3,47 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { RouterModule, Routes } from '@angular/router';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+// import { MatCardModule } from '@angular/material/card';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatExpansionModule } from '@angular/material/expansion';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+// HttpClient module for RESTful API
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './security/auth-guard';
+// import { LoginFormComponent } from './login-form/login-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationFormComponent,
+    LoginFormComponent,
+    DashboardComponent
   ],
-  imports: [
+  imports: [    
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    FormsModule, ReactiveFormsModule    
   ],
-  providers: [],
+  exports: [MatButtonModule,MatCardModule],
+  providers: [AuthGuard],
+  //HttpClientModule, , RegistrationClientService, LoginClientService
   bootstrap: [AppComponent]
 })
 export class AppModule { }
